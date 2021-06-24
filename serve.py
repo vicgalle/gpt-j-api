32856d26dd84f01c06793741c321f34c8e4d514d
Victor Gallego <Victor Gallego>
2021-06-24 15:57:28 +0000

    fix dir

--- serve.py
+++ serve.py
@@ -49,7 +49,7 @@ tokenizer = transformers.GPT2TokenizerFast.from_pretrained("gpt2")
 total_batch = per_replica_batch * jax.device_count() // cores_per_replica
 
 network = CausalTransformer(params)
-network.state = read_ckpt(network.state, "../step_383500/", devices.shape[1])
+network.state = read_ckpt(network.state, "./step_383500/", devices.shape[1])
 del network.state["opt_state"]
 network.state = network.move_xmap(network.state, np.zeros(cores_per_replica))
 
