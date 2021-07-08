23596d255aca0239453aca819cb53984ff88cd3d
Victor Gallego <vicgalle@ucm.es>
2021-07-08 20:08:39 +0200

    main app

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,44 +13,48 @@ def main():
     st.title("The GPT-J Workshop")
     """This app enables you to interact with large language models in a friendly way!"""
 
-    ex_names = [
-        "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.",
-    ]
-    example = st.selectbox("Choose an example prompt from this selector", ex_names)
+    with st.form(key="inputs"):
+        ex_names = [
+            "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.",
+        ]
+        example = st.selectbox("Choose an example prompt from this selector", ex_names)
 
-    inp = st.text_area(
-        "Or write your own prompt here!", example, max_chars=2000, height=150
-    )
-
-    with st.beta_expander("Generation options..."):
-        length = st.slider(
-            "Choose the length of the generated texts (in tokens)",
-            2,
-            1024,
-            512,
-            10,
-        )
-        temp = st.slider(
-            "Choose the temperature (higher - more random, lower - more repetitive)",
-            0.0,
-            1.5,
-            1.0,
-            0.05,
+        inp = st.text_area(
+            "Or write your own prompt here!", example, max_chars=2000, height=150
         )
 
-    payload = {
-        "context": inp,
-        "token_max_length": length,
-        "temperature": temp,
-        "top_p": 0.9,
-    }
+        with st.beta_expander("Generation options..."):
+            length = st.slider(
+                "Choose the length of the generated texts (in tokens)",
+                2,
+                1024,
+                512,
+                10,
+            )
+            temp = st.slider(
+                "Choose the temperature (higher - more random, lower - more repetitive)",
+                0.0,
+                1.5,
+                1.0,
+                0.05,
+            )
 
-    query = requests.post("http://api.vicgalle.net:5000/generate", params=payload)
-    response = query.json()
+        submit_button = st.form_submit_button(label="Generate!")
 
-    st.markdown(response["prompt"] + response["text"])
+    if submit_button:
 
-    time.sleep(1)
+        payload = {
+            "context": inp,
+            "token_max_length": length,
+            "temperature": temp,
+            "top_p": 0.9,
+        }
+
+        query = requests.post("http://api.vicgalle.net:5000/generate", params=payload)
+        response = query.json()
+
+        st.markdown(response["prompt"] + response["text"])
+        st.text(f"Generation done in {response['compute_time']:.3} s.")
 
     st.text("App baked with ❤️ by @vicgalle")
 
