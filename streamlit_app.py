af816a3af5cefc9482fa52fb1b333d9c415f8300
Victor Gallego <vicgalle@ucm.es>
2021-07-08 20:40:37 +0200

    finish app

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,48 +13,66 @@ def main():
     st.title("The GPT-J Workshop")
     """This app enables you to interact with large language models in a friendly way!"""
 
-    with st.form(key="inputs"):
-        ex_names = [
-            "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.",
-        ]
-        example = st.selectbox("Choose an example prompt from this selector", ex_names)
+    ex_names = [
+        "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.",
+        "asad adssad",
+    ]
+    example = st.selectbox("Choose an example prompt from this selector", ex_names)
 
-        inp = st.text_area(
-            "Or write your own prompt here!", example, max_chars=2000, height=150
+    inp = st.text_area(
+        "Or write your own prompt here!", example, max_chars=2000, height=150
+    )
+
+    with st.beta_expander("Generation options..."):
+        length = st.slider(
+            "Choose the length of the generated texts (in tokens)",
+            2,
+            1024,
+            512,
+            10,
+        )
+        temp = st.slider(
+            "Choose the temperature (higher - more random, lower - more repetitive)",
+            0.0,
+            1.5,
+            1.0,
+            0.05,
         )
 
-        with st.beta_expander("Generation options..."):
-            length = st.slider(
-                "Choose the length of the generated texts (in tokens)",
-                2,
-                1024,
-                512,
-                10,
-            )
-            temp = st.slider(
-                "Choose the temperature (higher - more random, lower - more repetitive)",
-                0.0,
-                1.5,
-                1.0,
-                0.05,
-            )
-
+    response = None
+    with st.form(key="inputs"):
         submit_button = st.form_submit_button(label="Generate!")
 
-    if submit_button:
+        if submit_button:
 
-        payload = {
-            "context": inp,
-            "token_max_length": length,
-            "temperature": temp,
-            "top_p": 0.9,
-        }
+            payload = {
+                "context": inp,
+                "token_max_length": length,
+                "temperature": temp,
+                "top_p": 0.9,
+            }
 
-        query = requests.post("http://api.vicgalle.net:5000/generate", params=payload)
-        response = query.json()
+            query = requests.post(
+                "http://api.vicgalle.net:5000/generate", params=payload
+            )
+            response = query.json()
 
-        st.markdown(response["prompt"] + response["text"])
-        st.text(f"Generation done in {response['compute_time']:.3} s.")
+            st.markdown(response["prompt"] + response["text"])
+            st.text(f"Generation done in {response['compute_time']:.3} s.")
+
+    if False:
+        col1, col2, *rest = st.beta_columns([1, 1, 10, 10])
+
+        def on_click_good():
+            response["rate"] = "good"
+            print(response)
+
+        def on_click_bad():
+            response["rate"] = "bad"
+            print(response)
+
+        col1.form_submit_button("👍", on_click=on_click_good)
+        col2.form_submit_button("👎", on_click=on_click_bad)
 
     st.text("App baked with ❤️ by @vicgalle")
 
