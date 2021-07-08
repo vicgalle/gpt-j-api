102df55ef8a545f3bee3efc2cf66879af5074f46
Victor Gallego <vicgalle@ucm.es>
2021-07-08 20:00:21 +0200

    sketch app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import time
+import requests
 
 
 def main():
+    st.set_page_config(  # Alternate names: setup_page, page, layout
+        layout="wide",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+        initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
+        page_title="The GPT-J Workshop",  # String or None. Strings get appended with "• Streamlit".
+        page_icon=None,  # String, anything supported by st.image, or None.
+    )
     st.title("The GPT-J Workshop")
     """This app enables you to interact with large language models in a friendly way!"""
 
@@ -11,7 +18,9 @@ def main():
     ]
     example = st.selectbox("Choose an example prompt from this selector", ex_names)
 
-    inp = st.text_area("Or write your own prompt here!", example, max_chars=2000)
+    inp = st.text_area(
+        "Or write your own prompt here!", example, max_chars=2000, height=150
+    )
 
     with st.beta_expander("Generation options..."):
         length = st.slider(
@@ -29,7 +38,17 @@ def main():
             0.05,
         )
 
-    st.subheader("Search results:")
+    payload = {
+        "context": inp,
+        "token_max_length": length,
+        "temperature": temp,
+        "top_p": 0.9,
+    }
+
+    query = requests.post("http://api.vicgalle.net:5000/generate", params=payload)
+    response = query.json()
+
+    st.markdown(response["prompt"] + response["text"])
 
     time.sleep(1)
 
